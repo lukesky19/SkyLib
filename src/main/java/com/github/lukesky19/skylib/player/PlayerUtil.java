7471d1a299e75a2727591755f2158e46cb63db05
lukeskywlker19 <thelukeskywlker19@gmail.com>
2024-12-22 14:38:53 -0500

    Improve the giveItem method and fix an issue with partial stack sizes.
    Due to a bug in the 1.21 and 1.21.1 API, there is a dedicated giveItem method for those versions.
    The method will now parse the server version and run the appropriate method based on the server's Minecraft version.
    
    The bug only applies to 1.21 and 1.21.1, so there is no reason to run extra code for no reason on other versions.

--- src/main/java/com/github/lukesky19/skylib/player/PlayerUtil.java
+++ src/main/java/com/github/lukesky19/skylib/player/PlayerUtil.java
@@ -1,7 +1,11 @@
 package com.github.lukesky19.skylib.player;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
-import org.bukkit.entity.Player;
+import com.github.lukesky19.skylib.version.VersionUtil;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -51,30 +55,89 @@ public class PlayerUtil {
     }
 
     /**
-     * Attempts to add an ItemStack to the player's inventory and if it can't, it will instead drop it at the player's feet.
-     * @param player A Bukkit Player
-     * @param itemStack A Bukkit ItemStack
-     * @param amount The amount of items to give
+     * Gives an item to a player and drops any items that can't be added on the ground.
+     * @param inventory The Inventory to add the ItemStack to.
+     * @param addStack The ItemStack to add.
+     * @param amount The amount of items to add.
+     * @param location The location to drop any items that don't fit inside the inventory.
      */
-    public static void giveItem(@NotNull Player player, @NotNull ItemStack itemStack, int amount) {
-        if(amount > itemStack.getMaxStackSize()) {
-            int count = amount / itemStack.getMaxStackSize();
-            itemStack.setAmount(itemStack.getMaxStackSize());
+    public static void giveItem(Inventory inventory, ItemStack addStack, int amount, Location location) {
+        if(VersionUtil.getMajorVersion() >= 21 && VersionUtil.getMinorVersion() < 3) {
+            giveItem1_21(inventory, addStack, amount, location);
+        } else {
+            addStack.setAmount(amount);
 
-            for(int i = 1; i <= count; i++) {
-                HashMap<Integer, ItemStack> leftover = player.getInventory().addItem(itemStack);
-                for(Map.Entry<Integer, ItemStack> leftoverEntry : leftover.entrySet()) {
-                    ItemStack leftoverStack = leftoverEntry.getValue();
-                    player.getWorld().dropItem(player.getLocation(), leftoverStack);
+            giveItem(inventory, addStack, location);
+            // appropriate
+        }
+    }
+
+    /**
+     * Gives an item to a player and drops any items that can't be added on the ground.
+     * This works for all versions except 1.21 and 1.21.1 due to a bug in the API that is fixed in 1.21.3 and newer.
+     * @param inventory The Inventory to add the item to.
+     * @param addStack The ItemStack to add.
+     * @param location The location to drop items if the inventory is full.
+     */
+    private static void giveItem(Inventory inventory, ItemStack addStack, Location location) {
+        // Try to add the item stack to the inventory
+        HashMap<Integer, ItemStack> leftover = inventory.addItem(addStack);
+
+        // If there are leftover items, we need to handle them
+        if (!leftover.isEmpty()) {
+            // Drop the leftover items on the ground
+            for (ItemStack item : leftover.values()) {
+                if (item.getAmount() > 0) {
+                    dropItem(location.getWorld(), location, item);
                 }
             }
-        } else {
-            itemStack.setAmount(amount);
-            HashMap<Integer, ItemStack> leftover = player.getInventory().addItem(itemStack);
-            for(Map.Entry<Integer, ItemStack> leftoverEntry : leftover.entrySet()) {
-                ItemStack leftoverStack = leftoverEntry.getValue();
-                player.getWorld().dropItem(player.getLocation(), leftoverStack);
+        }
+    }
+
+    /**
+     * Gives an item to a player.
+     * This handles stack sizes because of a bug in the api for versions 1.21 and 1.21.1 with the new stack size component.
+     * @param inventory The Inventory to add items to.
+     * @param itemStack The ItemStack to add.
+     * @param amount The amount of items to add.
+     * @param location The location to drop items if the inventory is full.
+     */
+    private static void giveItem1_21(@NotNull Inventory inventory, @NotNull ItemStack itemStack, int amount, Location location) {
+        int maxStackSize = itemStack.getMaxStackSize();
+        int remainder = amount % maxStackSize;
+        int stacks = (amount - remainder) / maxStackSize;
+
+        if(stacks == 0 && remainder == 0) return;
+
+        if(stacks >= 1) {
+            final ItemStack addStack = itemStack.clone();
+            addStack.setAmount(maxStackSize);
+
+            for(int i = 1; i <= stacks; i++) {
+                giveItem(inventory, addStack, location);
             }
+        }
+
+        if(remainder >= 1) {
+            final ItemStack addStack = itemStack.clone();
+            addStack.setAmount(remainder);
+
+            for(int i = 1; i <= stacks; i++) {
+                giveItem(inventory, addStack, location);
+            }
+        }
+    }
+
+    /**
+     * Drops an item stack at the specified location.
+     * @param world    The world where the item will be dropped.
+     * @param location The location where the item will be dropped.
+     * @param itemStack The item stack to drop.
+     */
+    private static void dropItem(World world, Location location, ItemStack itemStack) {
+        if (world != null && location != null && itemStack != null && itemStack.getAmount() > 0) {
+            Item item = world.dropItem(location, itemStack);
+            item.setPickupDelay(0); // Optional: Set pickup delay to 0 so players can pick it up immediately
         }
     }
 }
