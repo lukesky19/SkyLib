5b89630b5ca0656c41f7ec7b8f28b7e05620a010
lukeskywlker19 <thelukeskywlker19@gmail.com>
2025-06-04 22:20:57 -0400

    Add a check to ensure SkyLib is running on version 1.21.4 or newer. Also add some extra comments and a javadoc for the default constructor.

--- src/main/java/com/github/lukesky19/skylib/SkyLib.java
+++ src/main/java/com/github/lukesky19/skylib/SkyLib.java
@@ -17,6 +17,7 @@
 */
 package com.github.lukesky19.skylib;
 
+import com.github.lukesky19.skylib.adventure.AdventureUtil;
 import com.github.lukesky19.skylib.listener.LoginListener;
 import com.github.lukesky19.skylib.version.VersionUtil;
 import io.papermc.paper.ServerBuildInfo;
@@ -27,6 +28,11 @@ import org.jetbrains.annotations.NotNull;
  * Entry point to the plugin.
  */
 public final class SkyLib extends JavaPlugin {
+    /**
+     * This is the entry point to the plugin.
+     */
+    public SkyLib() {}
+
     @Override
     public void onEnable() {
         // Get ServerBuildInfo and Minecraft version
@@ -43,6 +49,14 @@ public final class SkyLib extends JavaPlugin {
         VersionUtil.setMajorVersion(Integer.parseInt(splitVersion[1]));
         VersionUtil.setMinorVersion(Integer.parseInt(splitVersion[2]));
 
+        // Ensure SkyLib is running on Minecraft version 1.21.4 or newer.
+        if(VersionUtil.getMajorVersion() < 21 || VersionUtil.getMinorVersion() < 4) {
+            this.getComponentLogger().error(AdventureUtil.serialize("SkyLib version 1.3.0.0 and newer only works on Minecraft Version 1.21.4 and newer."));
+            this.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        // Register Listener(s)
         this.getServer().getPluginManager().registerEvents(new LoginListener(), this);
     }
 }
